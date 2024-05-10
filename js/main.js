e75c9067e687caee80eaa4f815022e111090d02d
AntonioDattolo <antonio19794.a@gmail.com>
2024-05-10 18:29:36 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue
+
+const myConfig = {
+	
+	data() {
+		
+		return {
+			message: 'Welcome to VueJS!',
+			classContainer : "my-3 p-0 container d-flex justify-content-center align-items-center flex-wrap",
+			classTitle : "effect my-2 col-12",
+			effect : "effectInner text-center d-block"
+		}
+	},
+	methods: {
+		
+	}
+};
+
+let app = Vue.createApp(myConfig);
+app.mount('#app');
+document.getElementById("img").src= './img/vuejs-devlopment-main.jpg'
